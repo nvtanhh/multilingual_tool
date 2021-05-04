23056761e7f43c8c17d5dbb78f71729e9de5a7cd
Spider Man <66580316+nvtanhh@users.noreply.github.com>
2021-05-04 11:37:08 +0700

    get rid of errors when exist {variables} in String

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import glob
 import json
 from pathlib import Path
 from google_trans_new import google_translator
+import re
+
+translator = google_translator()
 
 
 def startTranslate(srcFolder, desFolder, srcLang, desLang):
 
     Path(desFolder).mkdir(parents=True, exist_ok=True)
-
-    translator = google_translator()
 
     files = []
     for file in glob.glob(srcFolder+"/*.arb"):
@@ -20,10 +21,24 @@ def startTranslate(srcFolder, desFolder, srcLang, desLang):
             data = json.load(json_file)
             for key, value in data.items():
                 if key.find('@') == -1:
-                    data[key] = translator.translate(
-                        value, lang_src=srcLang, lang_tgt=desLang).strip()
+                    data[key] = _translateAndProcess(
+                        data[key], srcLang, desLang)
             with open(desFolder+'/'+file[file.rindex('\\')+1:], 'w+',  encoding='utf8') as outfile:
                 json.dump(data, outfile, ensure_ascii=False, indent=4,)
+
+
+def _translateAndProcess(text, srcLang, desLang):
+    translatedText = translator.translate(
+        text, lang_src=srcLang, lang_tgt=desLang).strip()
+    if translatedText.find('{') == -1:
+        return translatedText
+    else:
+        pattern = re.compile(r'{(.+?)}')
+        srcVars = re.findall(pattern, text)
+        desVars = re.findall(pattern, translatedText)
+        for i in range(len(srcVars)):
+            translatedText = translatedText.replace(desVars[i], srcVars[i])
+        return translatedText
 
 
 if __name__ == "__main__":
